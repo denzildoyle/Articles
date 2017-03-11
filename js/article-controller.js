8198864b69ee07b3798adb4100c0cbd1d5c9ee09
denzildoyle <yell@denzildoyle.me>
2017-03-11 12:55:54 -0400

    Add git ignore

--- js/article-controller.js
+++ js/article-controller.js
@@ -1,5 +1,11 @@
 var app = angular.module('app', []);
 
 app.controller('articleCtrl', function($scope, $location, $http) {
-    $scope.recommendations = $http.post("https://getpocket.com/v3/get?consumer_key=64324-50c92e1b5146fdf04e9671f7&access_token=6672e060-6f27-1c01-057a-939be6&state=archive&favorite=1&sort=newest&detailType=complete"); 
+
+	var url = "https://getpocket.com/v3/get?consumer_key=64324-50c92e1b5146fdf04e9671f7&access_token=6672e060-6f27-1c01-057a-939be6&state=archive&favorite=1&sort=newest&detailType=complete";
+
+    $http.get(url)
+        .then(function(response){
+            $scope.recommendations = response.data.list;
+        });
 });
