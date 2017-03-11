809607e04aa368858a643e2c71858435a3dd8c08
denzildoyle <yell@denzildoyle.me>
2017-03-11 15:52:59 -0400

    Add filter to extract domain name only from url

--- js/article-controller.js
+++ js/article-controller.js
@@ -9,3 +9,11 @@ app.controller('articleCtrl', function($scope, $location, $http) {
             $scope.recommendations = response.data.list;
         });
 });
+
+app.filter( 'domain', function () {
+	return function ( input ) {
+		var parser = document.createElement('a');
+		parser.href = input;
+		return parser.hostname;
+	};
+});
