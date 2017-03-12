bd05d59c5435fff337d10a635083db3a93cb2e54
denzildoyle <yell@denzildoyle.me>
2017-03-12 15:16:11 -0400

    Organize recommendations

--- js/article-controller.js
+++ js/article-controller.js
@@ -11,9 +11,27 @@ app.controller('articleCtrl', function($scope, $location, $http) {
 });
 
 app.filter( 'domain', function () {
-	return function ( input ) {
+	return function (input) {
 		var parser = document.createElement('a');
 		parser.href = input;
 		return parser.hostname;
 	};
+});
+
+app.filter('toArray', function () {
+  return function (obj, addKey) {
+    if (!angular.isObject(obj)) {return obj;}
+    if ( addKey === false ) {
+      return Object.keys(obj).map(function(key) {
+        return obj[key];
+      });
+    } else {
+      return Object.keys(obj).map(function (key) {
+        var value = obj[key];
+        return angular.isObject(value) ?
+          Object.defineProperty(value, '$key', { enumerable: false, value: key}) :
+          { $key: key, $value: value };
+      });
+    }
+  };
 });
