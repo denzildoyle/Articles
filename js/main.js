bfac9888adaeff029348a946bb355b56a2460b2f
denzildoyle <yell@denzildoyle.me>
2017-03-24 13:45:04 -0400

    Add thankyou and subscription pages

--- js/main.js
+++ js/main.js
@@ -6,14 +6,18 @@ $(document).ready(function() {
 	// get copyright year
 	$('#year').html(today.getFullYear());
 
-	$('#nav-toggle').on('click', function(ev) {
+	$('#nav-toggle').on('click', function(e) {
         $(this).toggleClass('active');
         $('#menu').toggleClass('active');
         $('.mobile-nav').toggleClass('active');
-        ev.preventDefault();
+        
+        e.preventDefault();
             $('html, body').stop().animate({
             'scrollTop': $('#recommendation').offset().top
         }, 1000, 'swing');
+
+        e.stopPropagation();
+        window.scrollTo(0,0);
         
         $("#search").focus();
         $('body').toggleClass('no-overflow');
