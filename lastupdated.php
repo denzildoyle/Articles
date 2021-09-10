bdbc9ee9cac48908f106c6a0622362ce610b3746
Denzil Doyle <denzildoyle@gmail.com>
2021-09-10 08:22:31 -0400

    Update to allow for CORS request

--- lastupdated.php
+++ lastupdated.php
@@ -1,18 +1,26 @@
 <?php 
+
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Credentials: true");
+	header("Access-Control-Max-Age: 1000");
+	header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+	header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
+
 	// Get cURL resource
 	$curl = curl_init();
+	
 	// Set some options - we are passing in a useragent too here
 	curl_setopt_array($curl, array(
 	    CURLOPT_RETURNTRANSFER => 1,
 	    CURLOPT_URL => 'https://getpocket.com/v3/get?consumer_key=64324-50c92e1b5146fdf04e9671f7&access_token=6672e060-6f27-1c01-057a-939be6&state=archive&favorite=1&sort=newest&detailType=complete&count=1',
 	    CURLOPT_USERAGENT => 'Codular Sample cURL Request'
 	));
+
 	// Send the request & save response to $resp
 	$resp = curl_exec($curl);
 
-
 	print_r($resp);
 
-		// Close request to clear up some resources
+	// Close request to clear up some resources
 	curl_close($curl);
 ?>
